a288897b139b9614ed9e9e69edf34ddc0905666a
armen-melkonyan-23 <armen.melkonyan.a@cognaize.com>
2025-04-25 01:08:28 +0400

    Read templates with jinja2

--- agents/character_agents.py
+++ agents/character_agents.py
@@ -2,14 +2,15 @@ import os
 
 from agents.base_agent import GameAgent
 from src.config import CFG
+from src.utils import read_prompt_template
 
 
 def load_character_prompts(directory: str):
     prompts = {}
     for filename in os.listdir(directory):
         if filename.endswith(".j2"):
-            with open(os.path.join(directory, filename), 'r') as file:
-                prompts[filename[:-3]] = file.read()
+            character = filename.rsplit(".", maxsplit=1)[0]
+            prompts[character] = read_prompt_template(directory, character).render()
     return prompts
 
 character_prompts = load_character_prompts(CFG.character_templates_dir)
@@ -17,5 +18,3 @@ character_prompts = load_character_prompts(CFG.character_templates_dir)
 agents = {}
 for name, prompt in character_prompts.items():
     agents[name] = GameAgent(name=name, background_prompt=prompt)
-
-...
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+from typing import Union
+
 import jinja2
 
-def read_prompt_template(path: Path, name: str) -> jinja2.Template:
+
+def read_prompt_template(path: Union[Path, str], name: str) -> jinja2.Template:
     with open(path / f"{name}.j2") as f:
         return jinja2.Template(f.read())
